9297bb12955005cc8998ea5b1aa229ab7d3429b2
tanish <tanish.afre@gmail.com>
2022-08-01 10:54:25 +0100

    changing add memory and edit memory every time new memory is added

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,25 +1,41 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import useStyles from "./styles";
 import { TextField, Button, Typography, Paper,  } from "@material-ui/core";
 import FileBase from "react-file-base64";
-import {useDispatch} from "react-redux";
-import { createPost } from "../../actions/posts";
+import {useDispatch, useSelector} from "react-redux";
+import { createPost, updatePost } from "../../actions/posts";
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
+    
     const [postData, setPostData] = useState({
         creator: '', title: '', message: '', tags: '', selectedFiles: '',
     });
+    const post = useSelector((state) => currentId ? state.posts.find((p)=> p._id ===currentId) : null);
     const classes = useStyles();
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        if(post){
+            setPostData(post);
+        }
+    }, [post])
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        dispatch(createPost(postData));
+        if(currentId){
+            dispatch(updatePost(currentId,postData)); 
+        } else{
+            dispatch(createPost(postData));
+        }
+        clear();
+
+        
     }
 
     const clear = () => {
-
+        setCurrentId(null);
+        setPostData({creator: '', title: '', message: '', tags: '', selectedFiles: '',});
     }
 
 
@@ -27,7 +43,7 @@ const Form = () => {
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
                 <Typography varient="h6">
-                    Creating a memory
+                    {currentId ? `Editing` : `Creating`} a memory
                 </Typography>
                 <TextField 
                     name="creator" 
@@ -65,7 +81,7 @@ const Form = () => {
                     <FileBase 
                         type="file"
                         multiple={false}
-                        onDO={(base64) => setPostData({ ...postData, selectedFiles: base64})}
+                        onDo={(base64) => setPostData({ ...postData, selectedFiles: base64})}
                     />
                 </div>
                 <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>
