b9009d01d262f8c9ca3d54657605a27e100f73cf
tanish <tanish.afre@gmail.com>
2022-08-01 10:55:40 +0100

    adding cool background

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'; 
+import "./index.css";
 
 import reducers from "./reducers";
 
